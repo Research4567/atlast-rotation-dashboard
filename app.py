65c3746e2047f5d784a689e689458dfedbaf0d40
AT-LSST <monitormydata@gmail.com>
2026-02-24 15:17:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ MASTER_PATH = Path("master_results_clean.csv")  # required
 # -------------------------
 BQ_PROJECT = "lsst-484623"
 BQ_LOCATION = "US"  # change to "EU" if the dataset Details says EU
-BQ_DATASET = "asteroid_institute__mpc_replica_views"  # <-- DOUBLE underscore
+BQ_DATASET = "asteroid_institute___mpc_replica_views"  # <-- DOUBLE underscore
 BQ_TABLE   = "public_obs_sbn_clustered"
 BQ_STN     = "X05"
 BQ_ROW_LIMIT = 20000
@@ -834,6 +834,7 @@ else:
         mime="text/csv",
         use_container_width=True,
     )
+
 
 
 
